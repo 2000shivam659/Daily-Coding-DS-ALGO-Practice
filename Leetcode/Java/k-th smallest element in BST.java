3ac9d74a3bdabcdf6f5ed881ba1c7b46f92de522
Shivam Singh <2000shivam659@gmail.com>
2021-07-18 13:02:52 +0530

    Java-solution k-th smallest element in BST

---
+++ Leetcode/Java/k-th smallest element in BST.java
@@ -0,0 +1,48 @@
+// Question  https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+
+// Example 1:
+// Input: root = [3,1,4,null,2], k = 1
+// Output: 1
+
+// Example 2:
+// Input: root = [5,3,6,2,4,null,null,1], k = 3
+// Output: 3
+
+// Solution 
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ public int kthSmallest(TreeNode root, int k) {
+      Stack<TreeNode> st = new Stack<>();
+      
+      while (root != null) {
+          st.push(root);
+          root = root.left;
+      }
+          
+      while (k != 0) {
+          TreeNode n = st.pop();
+          k--;
+          if (k == 0) return n.val;
+          TreeNode right = n.right;
+          while (right != null) {
+              st.push(right);
+              right = right.left;
+          }
+      }
+      
+      return -1; // never hit if k is valid
+}
